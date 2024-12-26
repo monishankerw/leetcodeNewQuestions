bc7b89de37f40b8c9cdd94858343593cac95b1e2
monishankerw <monishanker121@gmail.com>
2024-12-26 08:15:02 +0530

    coding

---
+++ src/main/java/com/leetcode/leetcode/hibernates/Address.java
@@ -0,0 +1,26 @@
+package com.leetcode.leetcode.hibernates;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String city;
+    private String state;
+    private String zipcode;
+    @OneToOne(mappedBy = "address")
+
+    private Employee employee;
+
+}
---
+++ src/main/java/com/leetcode/leetcode/hibernates/Employee.java
@@ -0,0 +1,40 @@
+package com.leetcode.leetcode.hibernates;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double salary;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "employee_project", // Join table name
+            joinColumns = @JoinColumn(name = "employee_id"), // Foreign key for Employee
+            inverseJoinColumns = @JoinColumn(name = "project_id") // Foreign key for Project
+    )
+    private Set<Project> projects = new HashSet<>();
+}
